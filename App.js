d0eb7c78ac13554fe7a76be4b6df5faa3fd97185
Gabriel Guivisdalsky <101513911+santiagokasses@users.noreply.github.com>
2023-06-22 10:42:48 -0300

    terminado

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Card from './componentes/card'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={require('./imgs/central_perk_logo.jpeg')} style={styles.imagen}/>
       <Text style={styles.texto}>Try some of our most popular flavors!</Text>
+      <View style={{margin: '2rem'}}>
+      <Card/>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
